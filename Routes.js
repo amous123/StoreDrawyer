7cad441a78f88ba4132fee526eef1ec541e1b144
Sagal Maxamud <sagal@Sagals-MacBook-Air.local>
2019-07-17 13:44:58 -0400

    fixing merge issues

--- Routes.js
+++ Routes.js
@@ -6,10 +6,7 @@ import Signup from './pages/SignUpPage';
 import Yourwallet from './pages/YourWallet';
 import Cardview from './pages/CardView';
 import AddCard from './pages/AddCard';
-<<<<<<< HEAD
 import ScanCard from './pages/ScanCard';
-=======
->>>>>>> c3e1d2fbc0cb07198c69c81b27a911732b4ce796
 
 export default class Routes extends Component {
     render() {
@@ -25,11 +22,7 @@ export default class Routes extends Component {
                 <Scene key="login" component={Login} title="Login"/>
                 <Scene key="signup" component={Signup} title="Sign up"/>
                 <Scene key="cardview" component={Cardview} title="Cards"/>
-<<<<<<< HEAD
                 <Scene key="scancard" component={ScanCard} title="Scan Barcode"/>
-=======
-                <Scene key ="addcard" component = {AddCard} title = "Add Card"/>
->>>>>>> c3e1d2fbc0cb07198c69c81b27a911732b4ce796
                 </Stack>
             </Router>
         )
